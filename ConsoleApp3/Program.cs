faa9a085da14040c3823f09953c4892c5759d224
{{evildeveloper1}} <{{valerasarkisan285@gmail.com}}>
2023-12-04 19:49:53 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * class User
+ */
+class User
+{
+    private string login;
+    private string firstName;
+    private string lastName;
+    private int age;
+    private DateTime registrationDate;
+
+    public string Login
+    {
+        get { return login; }
+        set { login = value; }
+    }
+
+    public string FirstName
+    {
+        get { return firstName; }
+        set { firstName = value; }
+    }
+
+    public string LastName
+    {
+        get { return lastName; }
+        set { lastName = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public DateTime RegistrationDate
+    {
+        get { return registrationDate; }
+    }
+
+    public User(string login, string firstName, string lastName, int age)
+    {
+        this.login = login;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        Age = age;
+        registrationDate = DateTime.Now;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Login: {login}");
+        Console.WriteLine($"Name: {firstName}");
+        Console.WriteLine($"Surname: {lastName}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Registration date: {registrationDate}");
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,53 @@
+﻿enum Position
+{
+    Manager,
+    Developer,
+    Coder
+}
+
+class Employee
+{
+    private string lastName;
+    private string firstName;
+
+    public Employee(string lastName, string firstName)
+    {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public double CalculateSalary(Position position, int experience)
+    {
+        double baseSalary = 0;
+
+        switch (position)
+        {
+            case Position.Manager:
+                baseSalary = 30000;
+                break;
+            case Position.Developer:
+                baseSalary = 40000;
+                break;
+            case Position.Coder:
+                baseSalary = 35000;
+                break;
+        }
+
+        double experienceBonus = 0;
+
+        if (experience >= 5)
+        {
+            experienceBonus = 5000;
+        }
+
+        double salary = baseSalary + experienceBonus;
+        double tax = 0.15 * salary;
+        return salary - tax;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {lastName}");
+        Console.WriteLine($"Surname: {firstName}");
+    }
+}
